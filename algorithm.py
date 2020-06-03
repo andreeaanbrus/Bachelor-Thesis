63dacaa7463ad7b3e07bb6e2c0659e1808df5314
Andreea <andreeaanbrus@yahoo.com>
2020-06-04 00:38:39 +0300

    add results of samples

--- algorithm.py
+++ algorithm.py
@@ -19,23 +19,38 @@ class Algorithm:
         self.title = title
 
     def do(self):
+        f = open(self.summary_file, 'w+')
+
+        f.write(self.title)
+        sentences_to_write = self.input_text.split('\n')
+        for i in range(len(sentences_to_write)):
+            f.write('S' + str(i + 1) + ': ' + sentences_to_write[i] + '\n')
+
         noClusters = 3
         if self.compression == 30:
             noClusters = int(myround(len(self.input_text.split('.')) * 0.3))
         if self.compression == 50:
             noClusters = int(myround(len(self.input_text.split('.')) * 0.5))
         start1 = datetime.now()
-        (no_of_most_frequent_terms, termsFrequency, word_to_lemma, title_lemma) = termFrequency(self.input_text,
-                                                                                                self.title)
+        (no_of_most_frequent_terms, most_frequent_terms, word_to_lemma, title_lemma) = termFrequency(self.input_text,
+                                                                                                     self.title)
+        f.write('\n-------------MOST FREQUENT TERMS-------------\n')
+        for term in most_frequent_terms:
+            f.write(term + ' ')
+        f.write('\n')
         stop1 = datetime.now()
         print("Term frequency and lemmatization time: ", stop1 - start1)
         start2 = datetime.now()
-        sentences = removeSentences(self.input_text, '.')
-        mostFrequentTerms = termsFrequency[:no_of_most_frequent_terms]
+        sentences = removeSentences(self.input_text, '.\n')
+        mostFrequentTerms = most_frequent_terms[:no_of_most_frequent_terms]
 
         vectorRepresentationOfSentences(sentences, mostFrequentTerms,
                                         no_of_most_frequent_terms,
                                         word_to_lemma, title_lemma)
+        f.write('\n-----------VECTOR REPRESENTATION-------------\n')
+        for sentence in sentences:
+            f.write('S' + str(sentence.id) + ': ' + str(sentence.representation) + '    with rank ' + str(
+                sentence.rank) + '\n')
         stop2 = datetime.now()
         print("Vector representation time: ", stop2 - start2)
 
@@ -44,11 +59,15 @@ class Algorithm:
             if zero_vector(sentence.representation):
                 sentences.remove(sentence)
 
+        summary = ''
+        summary_positions = []
+
         # 6.Apply the hierarchical clustering algorithm for T = {S1; … ; Sn}
         labels = None
         if self.method == 'hierarchical':
             start3 = datetime.now()
-            cluster = MyHierarchicalClustering(noClusters=noClusters, similarity=customSimilarity, input=[sentence.representation for sentence in sentences])
+            cluster = MyHierarchicalClustering(noClusters=noClusters, similarity=cosine,
+                                               input=[sentence.representation for sentence in sentences])
             labels = cluster.predict()
             stop3 = datetime.now()
             print("Clustering time: ", stop3 - start3)
@@ -61,9 +80,18 @@ class Algorithm:
 
         for i in range(len(labels)):
             sentences[i].label = labels[i]
+        if self.method == 'kmeans':
+            f.write('\n-------CLUSTERS KMEANS--------\n')
+        if self.method == 'hierarchical':
+            f.write('\n-------CLUSTERS HIERARCHICAL-----\n')
+        for i in range(noClusters):
+            f.write('c' + str(i) + ' ')
+            for sentence in sentences:
+                if sentence.label == i:
+                    f.write(str(sentence.id) + ', ')
+            f.write('\n')
 
         # 7.Construct the summary
-        summary = ''
         summary_positions = [-100 for _ in range(noClusters)]
         summary_rank = [-100 for _ in range(noClusters)]
         for sentence in sentences:
@@ -73,10 +101,18 @@ class Algorithm:
         # sort the summary_positions to assure the chronological order of the sentences
         summary_positions.sort()
         print(summary_positions)  # <- the positions in the initial sentences
+
+        if self.method == 'kmeans':
+            f.write('\n-----LABELS---------\n')
+            f.write(str(labels) + '\n')
+            f.write('\n-----CENTROIDS------\n')
+            for centroid in centroids:
+                f.write(str(centroid) + '\n')
         for sentence in sentences:
             if sentence.id in summary_positions:
                 summary += sentence.text
-        fout = open(self.summary_file, "w")  # write summary here
-        fout.write(summary)
-        fout.close()
+        f.close()
+        # fout = open(self.summary_file, "w")  # write summary here
+        # fout.write(summary)
+        # fout.close()
         return sentences, summary, summary_positions
--- evaluation.py
+++ evaluation.py
@@ -1,17 +1,9 @@
-import math
-
 from algorithm import Algorithm
-
-
-def myround(x):
-    decimal, integer = math.modf(x)
-    if decimal >= 0.5:
-        return integer + 1
-    return integer
+from myutils import myround
 
 
 def testNumberSentences():
-    for i in range(1, 13):
+    for i in range(1, 16):
         manual_extract_file_m = 'testdata/samples/AVM-summary' + str(i) + '.txt'
         manual_extract_file_t = 'testdata/samples/AIS-summary' + str(i) + '.txt'
         inputFile = 'testdata/samples/sample' + str(i) + '.txt'
@@ -28,12 +20,12 @@ def testNumberSentences():
             noClusters50 = myround((len(input_text.split('.')) - 1) * 0.5)
             noClusters30 = myround((len(input_text.split('.')) - 1) * 0.3)
             print(i)
-            if len(manual_extract_30_m) != noClusters30:
-                print("Mami30: ", len(manual_extract_30_m) == noClusters30, " actual: ", noClusters30, "result: ",
-                      len(manual_extract_30_m))
-            if len(manual_extract_50_m) != noClusters50:
-                print("Mami50: ", len(manual_extract_50_m) == noClusters50, " actual: ", noClusters50, "result: ",
-                      len(manual_extract_50_m))
+            # if len(manual_extract_30_m) != noClusters30:
+            print("Mami30: ", len(manual_extract_30_m) == noClusters30, " actual: ", noClusters30, "result: ",
+                  len(manual_extract_30_m))
+            # if len(manual_extract_50_m) != noClusters50:
+            print("Mami50: ", len(manual_extract_50_m) == noClusters50, " actual: ", noClusters50, "result: ",
+                  len(manual_extract_50_m))
             if len(manual_extract_30_t) != noClusters30:
                 print("Tati30: ", len(manual_extract_30_t) == noClusters30, " actual: ", noClusters30, "result: ",
                       len(manual_extract_30_t))
@@ -41,35 +33,228 @@ def testNumberSentences():
                 print("Tati50: ", len(manual_extract_50_t) == noClusters50, " actual: ", noClusters50, "result: ",
                       len(manual_extract_50_t))
 
+# testNumberSentences()
+def evaluate_pers1_vs_pers2_30():
+    for i in range(1, 16):
+        manual_extract_file_m = 'testdata/samples/AVM-summary' + str(i) + '.txt'
+        manual_extract_file_t = 'testdata/samples/AIS-summary' + str(i) + '.txt'
+        with open(manual_extract_file_m) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+            manual_extract_50_m = [int(number) for number in extract_file.readline().split(' ')]
+        with open(manual_extract_file_t) as extract_file:
+            manual_extract_30_t = [int(number) for number in extract_file.readline().split(' ')]
+            manual_extract_50_t = [int(number) for number in extract_file.readline().split(' ')]
+        similar_30 = 0
+        for item in manual_extract_30_t:
+            if item in manual_extract_30_m:
+                similar_30 += 1
+        print((similar_30 * 100) / len(manual_extract_30_m))
 
-def main():
-    sampleNo = '2'
-    method = 'hierarchical'
-    compression = 50
-    inputFile = 'testdata/samples/sample' + sampleNo + '.txt'
-    summaryFile = 'testdata/samples/summary-input' + sampleNo + '.txt'
-    with open(inputFile) as f:
-        title = next(f)
-        input_text = f.read()
-    algorithm = Algorithm(input_text, method, compression, summaryFile, title)
-    sentences_algo, summaryResponse = algorithm.do()
 
-    manual_extract_file = 'testdata/samples/AIS-summary' + sampleNo + '.txt'
-    with open(manual_extract_file) as extract_file:
-        manual_extract_30 = [int(number) for number in extract_file.readline().split(' ')]
-        manual_extract_50 = [int(number) for number in extract_file.readline().split(' ')]
-    print(manual_extract_30)
-    print(manual_extract_50)
+def evaluate_pers1_vs_pers2_50():
+    for i in range(1, 16):
+        manual_extract_file_m = 'testdata/samples/AVM-summary' + str(i) + '.txt'
+        manual_extract_file_t = 'testdata/samples/AIS-summary' + str(i) + '.txt'
+        with open(manual_extract_file_m) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+            manual_extract_50_m = [int(number) for number in extract_file.readline().split(' ')]
+        with open(manual_extract_file_t) as extract_file:
+            manual_extract_30_t = [int(number) for number in extract_file.readline().split(' ')]
+            manual_extract_50_t = [int(number) for number in extract_file.readline().split(' ')]
+        similar_50 = 0
+        for item in manual_extract_50_t:
+            if item in manual_extract_50_m:
+                similar_50 += 1
+        print((similar_50 * 100) / len(manual_extract_50_m))
 
-    sentences = input_text.split('.')
-    manual_extract_sentences = [sentences[i - 1] for i in manual_extract_50]  # array starts from 0
-    with open(summaryFile) as summaryFile:
-        automatic_summary = summaryFile.read().split('.')
-        print(automatic_summary)
 
-    for s in manual_extract_sentences:
-        print(s, s in automatic_summary)
+def evaluate_30_HAC_PERS1():
+    for i in range(1, 16):
+        manual_extract_file_m = 'testdata/samples/AVM-summary' + str(i) + '.txt'
+        input_text_file = 'testdata/samples/sample' + str(i) + '.txt'
+        output_HAC_cosine = 'testdata/samples/' + str(i) + '_HAC_cosine_30.txt'
+        # read input file
+        with open(input_text_file) as f:
+            title = next(f)
+            input_text = f.read()
+        # read manual extract
+        with open(manual_extract_file_m) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+        algorithm_30_HAC_cosine = Algorithm(input_text, 'hierarchical', 30, output_HAC_cosine, title)
+        sentences, summaryResponse, summary_sentences_indexes_30 = algorithm_30_HAC_cosine.do()
+        same_sentences = 0
+        for item in summary_sentences_indexes_30:
+            if item in manual_extract_30_m:
+                same_sentences += 1
+        print((same_sentences * 100) / len(manual_extract_30_m))
 
-    return
 
-main()
+def evaluate_50_HAC_PERS1():
+    for i in range(1, 16):
+        manual_extract_file_m = 'testdata/samples/AVM-summary' + str(i) + '.txt'
+        input_text_file = 'testdata/samples/sample' + str(i) + '.txt'
+        output_HAC_cosine = 'testdata/samples/' + str(i) + '_HAC_cosine_50' + '.txt'
+        # read input file
+        with open(input_text_file) as f:
+            title = next(f)
+            input_text = f.read()
+        # read manual extract
+        with open(manual_extract_file_m) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+            manual_extract_50_m = [int(number) for number in extract_file.readline().split(' ')]
+        algorithm_50_HAC_cosine = Algorithm(input_text, 'hierarchical', 50, output_HAC_cosine, title)
+        sentences, summaryResponse, summary_sentences_indexes_50 = algorithm_50_HAC_cosine.do()
+        same_sentences = 0
+        print("Automatic Summary ", str(i), summary_sentences_indexes_50)
+        for item in summary_sentences_indexes_50:
+            if item in manual_extract_50_m:
+                same_sentences += 1
+        print((same_sentences * 100) / len(manual_extract_50_m))
+
+
+def evaluate_30_HAC_PERS2():
+    for i in range(1, 16):
+        manual_extract_file_t = 'testdata/samples/AIS-summary' + str(i) + '.txt'
+        input_text_file = 'testdata/samples/sample' + str(i) + '.txt'
+        output_HAC_cosine = 'testdata/samples/summary_HAC_cosine'
+        # read input file
+        with open(input_text_file) as f:
+            title = next(f)
+            input_text = f.read()
+        # read manual extract
+        with open(manual_extract_file_t) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+        algorithm_30_HAC_cosine = Algorithm(input_text, 'hierarchical', 30, output_HAC_cosine, title)
+        sentences, summaryResponse, summary_sentences_indexes_30 = algorithm_30_HAC_cosine.do()
+        same_sentences = 0
+        print("Automatic Summary ", str(i), summary_sentences_indexes_30)
+        for item in summary_sentences_indexes_30:
+            if item in manual_extract_30_m:
+                same_sentences += 1
+        print((same_sentences * 100) / len(manual_extract_30_m))
+
+
+def evaluate_50_HAC_PERS2():
+    for i in range(1, 16):
+        manual_extract_file_t = 'testdata/samples/AIS-summary' + str(i) + '.txt'
+        input_text_file = 'testdata/samples/sample' + str(i) + '.txt'
+        output_HAC_cosine = 'testdata/samples/summary_HAC_cosine'
+        # read input file
+        with open(input_text_file) as f:
+            title = next(f)
+            input_text = f.read()
+        # read manual extract
+        with open(manual_extract_file_t) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+            manual_extract_50_m = [int(number) for number in extract_file.readline().split(' ')]
+        algorithm_50_HAC_cosine = Algorithm(input_text, 'hierarchical', 50, output_HAC_cosine, title)
+        sentences, summaryResponse, summary_sentences_indexes_50 = algorithm_50_HAC_cosine.do()
+        print("Automatic Summary ", str(i), summary_sentences_indexes_50)
+        same_sentences = 0
+        for item in summary_sentences_indexes_50:
+            if item in manual_extract_50_m:
+                same_sentences += 1
+        print((same_sentences * 100) / len(manual_extract_50_m))
+
+
+def evaluate_KMEANS_30_PERS1():
+    for i in range(1, 16):
+        print("Automatic Summary ", str(i))
+        manual_extract_file_t = 'testdata/samples/AVM-summary' + str(i) + '.txt'
+        input_text_file = 'testdata/samples/sample' + str(i) + '.txt'
+        output_HAC_cosine = 'testdata/samples/' + str(i) + '_KMEANS_30.txt'
+        # read input file
+        with open(input_text_file) as f:
+            title = next(f)
+            input_text = f.read()
+        # read manual extract
+        with open(manual_extract_file_t) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+        algorithm_50_HAC_cosine = Algorithm(input_text, 'kmeans', 30, output_HAC_cosine, title)
+        sentences, summaryResponse, summary_sentences_indexes_50 = algorithm_50_HAC_cosine.do()
+        same_sentences = 0
+        for item in summary_sentences_indexes_50:
+            if item in manual_extract_30_m:
+                same_sentences += 1
+        print((same_sentences * 100) / len(manual_extract_30_m))
+
+
+def evaluate_KMEANS_30_PERS2():
+    for i in range(1, 16):
+        print("Automatic Summary ", str(i))
+        manual_extract_file_t = 'testdata/samples/AIS-summary' + str(i) + '.txt'
+        input_text_file =  'testdata/samples/sample' + '.txt'
+        output_HAC_cosine = 'testdata/samples/' + str(i) + '_KMEANS.txt'
+
+        # read input file
+        with open(input_text_file) as f:
+            title = next(f)
+            input_text = f.read()
+        # read manual extract
+        with open(manual_extract_file_t) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+        algorithm_50_HAC_cosine = Algorithm(input_text, 'kmeans', 30, output_HAC_cosine, title)
+        sentences, summaryResponse, summary_sentences_indexes_50 = algorithm_50_HAC_cosine.do()
+        same_sentences = 0
+        for item in summary_sentences_indexes_50:
+            if item in manual_extract_30_m:
+                same_sentences += 1
+        print((same_sentences * 100) / len(manual_extract_30_m))
+
+
+def evaluate_KMEANS_50_PERS1():
+    for i in range(7, 16):
+        print("Automatic Summary ", str(i))
+        manual_extract_file_t = 'testdata/samples/AVM-summary' + str(i) + '.txt'
+        input_text_file = 'testdata/samples/sample' + str(i) + '.txt'
+        output_HAC_cosine = 'testdata/samples/summary_HAC_cosine'
+        # read input file
+        with open(input_text_file) as f:
+            title = next(f)
+            input_text = f.read()
+        # read manual extract
+        with open(manual_extract_file_t) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+            manual_extract_50_m = [int(number) for number in extract_file.readline().split(' ')]
+        algorithm_50_HAC_cosine = Algorithm(input_text, 'kmeans', 50, output_HAC_cosine, title)
+        sentences, summaryResponse, summary_sentences_indexes_50 = algorithm_50_HAC_cosine.do()
+        same_sentences = 0
+        for item in summary_sentences_indexes_50:
+            if item in manual_extract_50_m:
+                same_sentences += 1
+        print((same_sentences * 100) / len(manual_extract_50_m))
+
+
+def evaluate_KMEANS_50_PERS2():
+    for i in range(1, 16):
+        print("Automatic Summary ", str(i))
+        manual_extract_file_t = 'testdata/samples/AIS-summary' + str(i) + '.txt'
+        input_text_file = 'testdata/samples/sample' + str(i) + '.txt'
+        output_HAC_cosine = 'testdata/samples/' + str(i) + '_KMEANS_50.txt'
+        # read input file
+        with open(input_text_file) as f:
+            title = next(f)
+            input_text = f.read()
+        # read manual extract
+        with open(manual_extract_file_t) as extract_file:
+            manual_extract_30_m = [int(number) for number in extract_file.readline().split(' ')]
+            manual_extract_50_m = [int(number) for number in extract_file.readline().split(' ')]
+        algorithm_50_HAC_cosine = Algorithm(input_text, 'kmeans', 50, output_HAC_cosine, title)
+        sentences, summaryResponse, summary_sentences_indexes_50 = algorithm_50_HAC_cosine.do()
+        same_sentences = 0
+        for item in summary_sentences_indexes_50:
+            if item in manual_extract_50_m:
+                same_sentences += 1
+        print((same_sentences * 100) / len(manual_extract_50_m))
+
+
+# evaluate_50_HAC_PERS2()
+# evaluate_30_HAC_PERS2()
+# evaluate_50_HAC_PERS1()
+# evaluate_30_HAC_PERS1()
+# while True:
+# evaluate_KMEANS_30_PERS1()
+# evaluate_KMEANS_30_PERS2()
+# evaluate_KMEANS_50_PERS1()
+evaluate_KMEANS_50_PERS2()
+# pers2_50, pers1_30, pers1_50
